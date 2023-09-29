3687f90e4aaa6804da8488f0057ee6e27e2bcb45
Mahammadjan <m.mehemmedcanov@gmail.com>
2023-09-29 20:39:43 +0400

    fixed code

--- src/main/java/org/example/PermissionManager.java
+++ src/main/java/org/example/PermissionManager.java
@@ -3,27 +3,14 @@ package org.example;
 public class PermissionManager {
     private PermissionLevel mCurrentLevel;
 
-    // Constructor with an initial value of USER
     public PermissionManager() {
         mCurrentLevel = PermissionLevel.USER;
     }
 
-    // Method to get the name of the current role
     public String getCurrentRoleName() {
-        switch (mCurrentLevel) {
-            case ADMIN:
-                return "Admin";
-            case DEVELOPER:
-                return "Developer";
-            case USER:
-                return "User";
-            default:
-                return "Unknown";
-        }
+        return mCurrentLevel.toString();
     }
 
-
-    // Method to set the current permission level
     public void setCurrentPermissionLevel(PermissionLevel level) {
         mCurrentLevel = level;
     }
--- src/test/java/PermissionManagerTest.java
+++ src/test/java/PermissionManagerTest.java
@@ -2,7 +2,6 @@ import org.example.PermissionLevel;
 import org.example.PermissionManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PermissionManagerTest {
@@ -10,42 +9,29 @@ public class PermissionManagerTest {
 
     @BeforeEach
     public void setUp() {
-        // Initialize a new PermissionManager instance before each test
         permissionManager = new PermissionManager();
     }
 
     @Test
     public void testGetCurrentRoleName() {
-        // Test the getCurrentRoleName method
         assertEquals("User", permissionManager.getCurrentRoleName());
 
-        // Change the permission level to ADMIN
         permissionManager.setCurrentPermissionLevel(PermissionLevel.ADMIN);
         assertEquals("Admin", permissionManager.getCurrentRoleName());
 
-        // Change the permission level to DEVELOPER
         permissionManager.setCurrentPermissionLevel(PermissionLevel.DEVELOPER);
         assertEquals("Developer", permissionManager.getCurrentRoleName());
     }
 
     @Test
     public void testSetCurrentPermissionLevel() {
-        // Test the setCurrentPermissionLevel method
         assertEquals("User", permissionManager.getCurrentRoleName());
 
-        // Change the permission level to DEVELOPER
         permissionManager.setCurrentPermissionLevel(PermissionLevel.DEVELOPER);
         assertEquals("Developer", permissionManager.getCurrentRoleName());
 
-        // Change the permission level to ADMIN
         permissionManager.setCurrentPermissionLevel(PermissionLevel.ADMIN);
         assertEquals("Admin", permissionManager.getCurrentRoleName());
     }
 
-    public void testUnknownPermissionLevel() {
-        PermissionLevel unknownPermissionLevel = mock(PermissionLevel.class);
-        when(unknownPermissionLevel.toString()).thenReturn("UNKNOWN");
-        permissionManager.setCurrentPermissionLevel(unknownPermissionLevel);
-        assertEquals("Unknown", permissionManager.getCurrentRoleName());
-    }
 }
